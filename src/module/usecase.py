888fcb45fd8ee2b1ad86e2842a997a6ac77fcde2
senalw <senaldulanjala@gmail.com>
2024-06-24 14:49:41 +0530

    fix: remove @dataclasa annotation from usecase.py

--- src/module/usecase.py
+++ src/module/usecase.py
@@ -1,11 +1,9 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from typing import Any
 
 from src.routers.v1.schema import Response
 
 
-@dataclass
 class UseCase(ABC):
     @abstractmethod
     def execute(self, request: Any) -> Response:
